423dd7974b7bb05522d0527eb4be019003be2f4a
soomrofaizan <soomrofaizan001@gmail.com>
2025-11-18 21:52:05 +0500

    initial project settings

---
+++ src/test/java/com/contractor/ContractorApplication/ContractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contractor.ContractorApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
